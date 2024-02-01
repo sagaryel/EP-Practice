197aa71387704523c6992e78762f6ce8bf79d603
SagarYelgondHyniva <103254780+sagaryelgondhyniva@users.noreply.github.com>
2024-02-01 16:11:52 +0530

    now  commit

--- src/Lamda/employee/employeeCreateHandler.js
+++ src/Lamda/employee/employeeCreateHandler.js
@@ -1,44 +1,52 @@
-const AWS = require('aws-sdk');
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const {
+    DynamoDBClient,
+    PutItemCommand,
+    UpdateItemCommand,
+  } = require('@aws-sdk/client-dynamodb');
+  const { marshall } = require('@aws-sdk/util-dynamodb');
+  
+const client = new DynamoDBClient();
 
-const createEmployee = async (event) => {
-    const requestBody = JSON.parse(event.body);
-
-    // Validate the input data (Add your validation logic)
-
-    const params = {
-        TableName: 'employee-Details-dev', 
-        Item: {
+const createEmployee = async (event) =>  async (event) => {
+    console.log("inside the create employee details");
+    const response = { statusCode: 200 };
+    try {
+      const requestBody = JSON.parse(event.body);
+      
+      // Check for required fields
+      if (!requestBody.empId.empId || !requestBody.firstName || !requestBody.lastName || !requestBody.dob || !requestBody.email || !requestBody.branchOffice) {
+        throw new Error('Required fields are missing.');
+      }
+  
+      const params = {
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Item: marshall({
             empId: requestBody.empId,
             firstName: requestBody.firstName,
             lastName: requestBody.lastName,
             dob: requestBody.dob,
             email: requestBody.email,
             branchOffice: requestBody.branchOffice
-        }
-    };
-
-    try {
-        await dynamoDB.put(params).promise();
-
-        // Return all parameters in the response
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'Employee created successfully',
-                employee: params.Item // Return all params of the created employee
-            })
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ 
-                message: 'Failed to certificate details.',
-                errorMsg: error.message,
-                errorStack: error.stack,
-            })
-        };
+         }),                                    
+      };
+      const createResult = await client.send(new PutItemCommand(params));
+      response.body = JSON.stringify({
+        message: 'Successfully created employee details.',
+        createResult,
+      });
+    } catch (e) {
+      console.error(e);
+      response.statusCode = 500;
+      response.body = JSON.stringify({
+        message: 'Failed to create employee details.',
+        errorMsg: e.message,
+        errorStack: e.stack,
+      });
     }
-};
+    return response;
+  };
 
-module.exports = createEmployee;
+  module.exports = {
+    createEmployee
+  };
+    
