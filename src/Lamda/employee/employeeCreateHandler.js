58a5bb9fdbabbf5109a6d8f03782d95247108cc1
SagarYelgondHyniva <103254780+sagaryelgondhyniva@users.noreply.github.com>
2024-02-01 14:58:20 +0530

    final  commit

--- src/Lamda/employee/employeeCreateHandler.js
+++ src/Lamda/employee/employeeCreateHandler.js
@@ -7,7 +7,8 @@ export const createEmployee = async (event) => {
     // Validate the input data (Add your validation logic)
 
     const params = {
-        TableName: 'employee-Details', 
+        TableName: 'employee-Details
+        ', 
         Item: {
             empId: requestBody.employeeId,
             firstName: requestBody.firstName,
