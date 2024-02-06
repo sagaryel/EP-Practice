5e0e3c268a2e9433cd4d7264692b594aeee81c3e
SagarYelgondHyniva <103254780+sagaryelgondhyniva@users.noreply.github.com>
2024-02-06 10:45:38 +0530

    final  commit

--- src/Lamda/employee/employeeCreateHandler.js
+++ src/Lamda/employee/employeeCreateHandler.js
@@ -6,70 +6,69 @@ const {
 const { marshall } = require('@aws-sdk/util-dynamodb');
 const moment = require('moment');
 const client = new DynamoDBClient();
-const {httpStatusCodes, httpStatusMessages} = require("../../environment/appconfig");
+const { httpStatusCodes, httpStatusMessages } = require("../environment/appconfig");
 const currentDate = Date.now();      // get the current date and time in milliseconds
 const formattedDate = moment(currentDate).format('YYYY-MM-DD HH:mm:ss');    //formating date
 
 
 const createEmployee = async (event) => {
   console.log("inside the create employee details");
-  const response = { statusCode: httpStatusCodes.SUCCESS};
+  const response = { statusCode: httpStatusCodes.SUCCESS };
   try {
-    const requestBody = JSON.parse(event.body);
+      const requestBody = JSON.parse(event.body);
 
-    // Check for required fields
-    const requiredFields = ['empId', 'firstName', 'lastName', 'dob', 'email', 'branchOffice'];
-    if (!requiredFields.every(field => requestBody[field])) {
-      throw new Error('Required fields are missing.');
-    }
+      // Check for required fields
+      const requiredFields = ['employeeId', 'firstName', 'lastName', 'dateOfBirth', 'officeEmailAddress', 'branchOffice'];
+      if (!requiredFields.every(field => requestBody[field])) {
+          throw new Error('Required fields are missing.');
+      }
 
-    const params = {
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      Item: marshall({
-        empId: requestBody.empId,
-        firstName: requestBody.firstName,
-        lastName: requestBody.lastName,
-        dob: requestBody.dob,
-        email: requestBody.email,
-        branchOffice: requestBody.branchOffice,
-        password: requestBody.password || null,
-        gender: requestBody.gender || null,
-        ssnNumber: requestBody.ssnNumber || null,
-        maritalStatus: requestBody.maritalStatus || null,
-        nationality: requestBody.nationality || null,
-        passportNumber: requestBody.passportNumber || null,
-        mobileNumber: requestBody.mobileNumber || null,
-        PermanentAddress: requestBody.PermanentAddress || null,
-        relation: requestBody.relation || null,
-        personalEmailAddress: requestBody.personalEmailAddress || null,
-        PresentAddress: requestBody.PresentAddress || null,
-        contactNumber: requestBody.contactNumber || null,
-        officeEmailAddress: requestBody.officeEmailAddress || null,
-        joiningDate: requestBody.joiningDate || null,
-        emergencyContactPerson: requestBody.emergencyContactPerson || null,
-        designation: requestBody.designation || null,
-        emergencyContactNumber: requestBody.emergencyContactNumber || null,
-        resignedDate: requestBody.resignedDate || null,
-        relievedDate: requestBody.relievedDate || null,
-        leaveStructure: requestBody.leaveStructure || null,
-        createdDateTime: formattedDate,
-        updatedDateTime: requestBody.updatedDateTime || null,
-        department: requestBody.department || null
-      }),
-    };
-    const createResult = await client.send(new PutItemCommand(params));
-    response.body = JSON.stringify({
-      message: httpStatusMessages.SUCCESSFULLY_CREATED_EMPLOYEE_DETAILS,
-      createResult,
-    });
+      const params = {
+          TableName: process.env.EMPLOYEE_TABLE,
+          Item: marshall({
+              employeeId: requestBody.employeeId,
+              firstName: requestBody.firstName,
+              lastName: requestBody.lastName,
+              dateOfBirth: requestBody.dateOfBirth,
+              officeEmailAddress: requestBody.officeEmailAddress,
+              branchOffice: requestBody.branchOffice,
+              password: requestBody.password || null,
+              gender: requestBody.gender || null,
+              ssnNumber: requestBody.ssnNumber || null,
+              maritalStatus: requestBody.maritalStatus || null,
+              nationality: requestBody.nationality || null,
+              passportNumber: requestBody.passportNumber || null,
+              mobileNumber: requestBody.mobileNumber || null,
+              permanentAddress: requestBody.permanentAddress || null,
+              contactPerson: requestBody.contactPerson || null,
+              personalEmailAddress: requestBody.personalEmailAddress || null,
+              presentAddress: requestBody.presentAddress || null,
+              contactNumber: requestBody.contactNumber || null,
+              joiningDate: requestBody.joiningDate || null,
+              emergencyContactPerson: requestBody.emergencyContactPerson || null,
+              designation: requestBody.designation || null,
+              emergencyContactNumber: requestBody.emergencyContactNumber || null,
+              resignedDate: requestBody.resignedDate || null,
+              relievedDate: requestBody.relievedDate || null,
+              leaveStructure: requestBody.leaveStructure || null,
+              createdDateTime: formattedDate,
+              updatedDateTime: requestBody.updatedDateTime || null,
+              department: requestBody.department || null
+          }),
+      };
+      const createResult = await client.send(new PutItemCommand(params));
+      response.body = JSON.stringify({
+          message: httpStatusMessages.SUCCESSFULLY_CREATED_EMPLOYEE_DETAILS,
+          createResult,
+      });
   } catch (e) {
-    console.error(e);
-    response.statusCode = httpStatusCodes.BAD_REQUEST;
-    response.body = JSON.stringify({
-      message: httpStatusMessages.FAILED_TO_CREATE_EMPLOYEE_DETAILS,
-      errorMsg: e.message,
-      errorStack: e.stack,
-    });
+      console.error(e);
+      response.statusCode = httpStatusCodes.BAD_REQUEST;
+      response.body = JSON.stringify({
+          message: httpStatusMessages.FAILED_TO_CREATE_EMPLOYEE_DETAILS,
+          errorMsg: e.message,
+          errorStack: e.stack,
+      });
   }
   return response;
 };
