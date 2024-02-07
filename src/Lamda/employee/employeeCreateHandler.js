812c362a4ec53d9662e1f1d5042b064a09e3fe97
SagarYelgondHyniva <103254780+sagaryelgondhyniva@users.noreply.github.com>
2024-02-07 10:22:23 +0530

    final  commit

--- src/Lamda/employee/employeeCreateHandler.js
+++ src/Lamda/employee/employeeCreateHandler.js
@@ -7,7 +7,7 @@ const {
 const { marshall } = require('@aws-sdk/util-dynamodb');
 const moment = require('moment');
 const client = new DynamoDBClient();
-const {httpStatusCodes, httpStatusMessages} = require("../../environment/appconfig");
+const { httpStatusCodes, httpStatusMessages } = require("../../environment/appconfig");
 const { validateEmployeeDetails } = require("../../validator/validateRequest");
 const currentDate = Date.now();      // get the current date and time in milliseconds
 const formattedDate = moment(currentDate).format('YYYY-MM-DD HH:mm:ss');    //formating date
@@ -17,82 +17,102 @@ const createEmployee = async (event) => {
   console.log("inside the create employee details");
   const response = { statusCode: httpStatusCodes.SUCCESS };
   try {
-      const requestBody = JSON.parse(event.body);
+    const requestBody = JSON.parse(event.body);
 
-      // Check for required fields
-      // const requiredFields = ['employeeId', 'firstName', 'lastName', 'dateOfBirth', 'officeEmailAddress', 'branchOffice'];
-      // if (!requiredFields.every(field => requestBody[field])) {
-      //     throw new Error('Required fields are missing.');
-      // }
-      if (!validateEmployeeDetails(requestBody)) {
-        throw new Error('Required fields are missing.');
+    // Check for required fields
+    // const requiredFields = ['employeeId', 'firstName', 'lastName', 'dateOfBirth', 'officeEmailAddress', 'branchOffice'];
+    // if (!requiredFields.every(field => requestBody[field])) {
+    //     throw new Error('Required fields are missing.');
+    // }
+    if (!validateEmployeeDetails(requestBody)) {
+      throw new Error('Required fields are missing.');
     }
 
     // Check if the employeeId already exists
     const employeeIdExists = await isEmployeeIdExists(requestBody.employeeId);
     if (employeeIdExists) {
-        throw new Error('EmployeeId already exists.');
+      throw new Error('EmployeeId already exists.');
     }
 
-      const params = {
-          TableName: process.env.EMPLOYEE_TABLE,
-          Item: marshall({
-              employeeId: requestBody.employeeId,
-              firstName: requestBody.firstName,
-              lastName: requestBody.lastName,
-              dateOfBirth: requestBody.dateOfBirth,
-              officeEmailAddress: requestBody.officeEmailAddress,
-              branchOffice: requestBody.branchOffice,
-              password: requestBody.password || null,
-              gender: requestBody.gender || null,
-              ssnNumber: requestBody.ssnNumber || null,
-              maritalStatus: requestBody.maritalStatus || null,
-              nationality: requestBody.nationality || null,
-              passportNumber: requestBody.passportNumber || null,
-              mobileNumber: requestBody.mobileNumber || null,
-              permanentAddress: requestBody.permanentAddress || null,
-              contactPerson: requestBody.contactPerson || null,
-              personalEmailAddress: requestBody.personalEmailAddress || null,
-              presentAddress: requestBody.presentAddress || null,
-              contactNumber: requestBody.contactNumber || null,
-              joiningDate: requestBody.joiningDate || null,
-              emergencyContactPerson: requestBody.emergencyContactPerson || null,
-              designation: requestBody.designation || null,
-              emergencyContactNumber: requestBody.emergencyContactNumber || null,
-              resignedDate: requestBody.resignedDate || null,
-              relievedDate: requestBody.relievedDate || null,
-              leaveStructure: requestBody.leaveStructure || null,
-              createdDateTime: formattedDate,
-              updatedDateTime: requestBody.updatedDateTime || null,
-              department: requestBody.department || null
-          }),
-      };
-      const createResult = await client.send(new PutItemCommand(params));
-      response.body = JSON.stringify({
-          message: httpStatusMessages.SUCCESSFULLY_CREATED_EMPLOYEE_DETAILS,
-          createResult,
-      });
+    // Check if the email address already exists
+    const emailExists = await isEmailExists(requestBody.officeEmailAddress);
+    if (emailExists) {
+      throw new Error('Email address already exists.');
+    }
+
+    const params = {
+      TableName: process.env.EMPLOYEE_TABLE,
+      Item: marshall({
+        employeeId: requestBody.employeeId,
+        firstName: requestBody.firstName,
+        lastName: requestBody.lastName,
+        dateOfBirth: requestBody.dateOfBirth,
+        officeEmailAddress: requestBody.officeEmailAddress,
+        branchOffice: requestBody.branchOffice,
+        password: requestBody.password || null,
+        gender: requestBody.gender || null,
+        ssnNumber: requestBody.ssnNumber || null,
+        maritalStatus: requestBody.maritalStatus || null,
+        nationality: requestBody.nationality || null,
+        passportNumber: requestBody.passportNumber || null,
+        mobileNumber: requestBody.mobileNumber || null,
+        permanentAddress: requestBody.permanentAddress || null,
+        contactPerson: requestBody.contactPerson || null,
+        personalEmailAddress: requestBody.personalEmailAddress || null,
+        presentAddress: requestBody.presentAddress || null,
+        contactNumber: requestBody.contactNumber || null,
+        joiningDate: requestBody.joiningDate || null,
+        emergencyContactPerson: requestBody.emergencyContactPerson || null,
+        designation: requestBody.designation || null,
+        emergencyContactNumber: requestBody.emergencyContactNumber || null,
+        resignedDate: requestBody.resignedDate || null,
+        relievedDate: requestBody.relievedDate || null,
+        leaveStructure: requestBody.leaveStructure || null,
+        createdDateTime: formattedDate,
+        updatedDateTime: requestBody.updatedDateTime || null,
+        department: requestBody.department || null
+      }),
+    };
+    const createResult = await client.send(new PutItemCommand(params));
+    response.body = JSON.stringify({
+      message: httpStatusMessages.SUCCESSFULLY_CREATED_EMPLOYEE_DETAILS,
+      createResult,
+    });
   } catch (e) {
-      console.error(e);
-      response.statusCode = httpStatusCodes.BAD_REQUEST;
-      response.body = JSON.stringify({
-          message: httpStatusMessages.FAILED_TO_CREATE_EMPLOYEE_DETAILS,
-          errorMsg: e.message,
-          errorStack: e.stack,
-      });
+    console.error(e);
+    response.statusCode = httpStatusCodes.BAD_REQUEST;
+    response.body = JSON.stringify({
+      message: httpStatusMessages.FAILED_TO_CREATE_EMPLOYEE_DETAILS,
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
   }
   return response;
 };
 
 // Function to check if employeeId already exists
 const isEmployeeIdExists = async (employeeId) => {
-      const params = {
-          TableName: process.env.EMPLOYEE_TABLE,
-          Key: { employeeId: { S: employeeId } }
-      };
-      const { Item } = await client.send(new GetItemCommand(params));
-       // If Item is not null, employeeId exists
+  const params = {
+    TableName: process.env.EMPLOYEE_TABLE,
+    Key: { employeeId: { S: employeeId } }
+  };
+  const { Item } = await client.send(new GetItemCommand(params));
+  // If Item is not null, employeeId exists
   return !!Item;
+};
+
+const isEmailExists = async (emailAddress) => {
+  const params = {
+    TableName: 'employee_details',
+    IndexName: 'officeEmailAddress-index', // Assuming 'officeEmailAddress' is indexed
+    KeyConditionExpression: 'officeEmailAddress = :email',
+    ExpressionAttributeValues: {
+      ':email': { S: emailAddress }
+    },
+    ProjectionExpression: 'officeEmailAddress'
+  };
+  const data = await client.query(params).promise();
+  return data.Items.length > 0; // If there are items, email exists
 };
 
 module.exports = {
