8631d6fbaae3836a61420aa569a348b3462d7ffd
SagarYelgondHyniva <103254780+sagaryelgondhyniva@users.noreply.github.com>
2024-02-07 16:13:07 +0530

    aaa  commit

--- src/validator/validateRequest.js
+++ src/validator/validateRequest.js
@@ -5,7 +5,7 @@ const validateEmployeeDetails = (requestBody) => {
     // Check if required fields are missing
     if (!employeeId || !firstName || !lastName || !dateOfBirth || !officeEmailAddress || !branchOffice) {
         return false;
-    } else if (!officeEmailAddress.endsWith('.hyniva.com')) {
+    } else if (!officeEmailAddress.endsWith('@hyniva.com')) {
         throw new Error('Invalid officeEmailAddress domain. It should end with ".hyniva.com".');
     }
     // You can add more specific validation logic for each field if needed
