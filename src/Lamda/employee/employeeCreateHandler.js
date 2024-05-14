fa26346000a6bfedff012e6a37e0374038eab41c
SagarYelgondHyniva <103254780+sagaryelgondhyniva@users.noreply.github.com>
2024-05-14 16:20:03 +0530

    yyy  commit

--- src/Lamda/employee/employeeCreateHandler.js
+++ src/Lamda/employee/employeeCreateHandler.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.PG_USERNAME,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  user: "postgres",
+  host: "localhost",
+  database: "employee",
+  password: "postgres",
+  port: 5432,
 });
 
 const getEmployee = async (event) => {
